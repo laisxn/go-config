2a68689e9fd37711fc6165a0ec25ac7522f50fff
laisxn <191120844@qq.com>
2022-09-09 22:12:57 +0800

    Create config.go

---
+++ config.go
@@ -0,0 +1,41 @@
+package go-config
+
+import (
+	"gopkg.in/ini.v1"
+	"strconv"
+	"strings"
+)
+
+var config *ini.File
+
+func init() {
+	c, err := ini.Load("config.ini")
+	if err != nil {
+		panic(err)
+	}
+
+	config = c
+}
+
+func Instance() *ini.File {
+	return config
+}
+
+func Get(key string) string {
+	needle := "."
+	if strings.Contains(key, needle) {
+		keyArr := strings.Split(key, needle)
+		return config.Section(keyArr[0]).Key(keyArr[1]).String()
+	}
+	return ""
+}
+
+func GetToBool(key string) bool {
+	rBool, _ := strconv.ParseBool(Get(key))
+	return rBool
+}
+
+func GetToInt(key string) int {
+	r, _ := strconv.Atoi(Get(key))
+	return r
+}
